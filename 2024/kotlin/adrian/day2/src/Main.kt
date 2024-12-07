828c755b4e08b5040a06ac0ea04e7c9f79f8433f
Adrian Jonsson Sjödin <adrian.jonsson.sjodin@omegapoint.se>
2024-12-07 14:36:34 +0100

    Day2 part 1 done

--- 2024/kotlin/adrian/day2/src/Main.kt
+++ 2024/kotlin/adrian/day2/src/Main.kt
@@ -1,3 +1,21 @@
+import java.io.File
+import kotlin.math.abs
+
 fun main() {
-    println("Hello World!")
+val fileName = "src/data.txt"
+
+
+    val reports = File(fileName).readLines().map { line -> line.split(" ").map { it.toInt() }.toIntArray() }
+    println(reports[0].contentToString())
+    val safeReports = reports.filter { report -> isSafeReport(report.toList())}
+println(safeReports.size)
+}
+
+fun isSafeReport(report: List<Int>): Boolean {
+    val isStrictlyIncreasing = report.zipWithNext().all { (a,b) -> a < b }
+    val isStrictlyDecreasing = report.zipWithNext().all { (a,b) -> a > b }
+    if (isStrictlyIncreasing || isStrictlyDecreasing) {
+       return report.zipWithNext().all { (a,b) -> abs(a-b) in 1..3 }
+    }
+    return false
 }
