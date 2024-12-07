2fdb2c6517e7a64b64c09574a1cab773823d0508
Adrian Jonsson Sjödin <adrian.jonsson.sjodin@omegapoint.se>
2024-12-07 17:01:56 +0100

    Day2 part 2 done

--- 2024/kotlin/adrian/day2/src/Main.kt
+++ 2024/kotlin/adrian/day2/src/Main.kt
@@ -2,20 +2,30 @@ import java.io.File
 import kotlin.math.abs
 
 fun main() {
-val fileName = "src/data.txt"
+    val fileName = "src/data.txt"
 
+    val reports: List<List<Int>> =
+        File(fileName).readLines().map { line -> line.split(" ").map { it.toInt() }.toList() }
+    val safeReports = reports.filter { report -> isSafeReport(report) }
+    val safeReportsWithDampner = reports.filter { report -> isSafeReportUsingProblemDampener(report) }
 
-    val reports = File(fileName).readLines().map { line -> line.split(" ").map { it.toInt() }.toIntArray() }
-    println(reports[0].contentToString())
-    val safeReports = reports.filter { report -> isSafeReport(report.toList())}
-println(safeReports.size)
+    println("Total number of safe reports: ${safeReports.size}")
+    println("Total number of safe reports with dampener: ${safeReportsWithDampner.size}")
 }
 
 fun isSafeReport(report: List<Int>): Boolean {
-    val isStrictlyIncreasing = report.zipWithNext().all { (a,b) -> a < b }
-    val isStrictlyDecreasing = report.zipWithNext().all { (a,b) -> a > b }
+    val isStrictlyIncreasing = report.zipWithNext().all { (a, b) -> a < b }
+    val isStrictlyDecreasing = report.zipWithNext().all { (a, b) -> a > b }
     if (isStrictlyIncreasing || isStrictlyDecreasing) {
-       return report.zipWithNext().all { (a,b) -> abs(a-b) in 1..3 }
+        return report.zipWithNext().all { (a, b) -> abs(a - b) in 1..3 }
     }
     return false
 }
+
+fun isSafeReportUsingProblemDampener(report: List<Int>): Boolean {
+    return report.indices.any {
+        val skipped = report.toMutableList().apply { removeAt(it) }
+        isSafeReport(skipped)
+    }
+}
+
